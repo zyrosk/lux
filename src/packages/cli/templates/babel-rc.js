53e257ae66370087e65d99e2da8aece100043d02
Zachary Golba <zachary.golba@postlight.com>
2016-06-04 15:37:44 -0400

    refactor: use lux babel preset

--- src/packages/cli/templates/babel-rc.js
+++ src/packages/cli/templates/babel-rc.js
@@ -6,18 +6,6 @@ import template from '../../template';
  */
 export default (): string => template`
   {
-    "plugins": [
-      "external-helpers-2",
-      "syntax-trailing-function-commas",
-      "transform-decorators-legacy",
-      "transform-class-properties",
-      "transform-decorators",
-      "transform-es2015-destructuring",
-      "transform-es2015-parameters",
-      "transform-es2015-spread",
-      "transform-object-rest-spread",
-      "transform-async-to-generator",
-      "transform-exponentiation-operator"
-    ]
+    "presets": ["lux"]
   }
 `;
--- src/packages/cli/templates/package-json.js
+++ src/packages/cli/templates/package-json.js
@@ -1,9 +1,9 @@
 // @flow
-import { version } from '../../../../package.json';
-
+import { version, devDependencies } from '../../../../package.json';
 import template from '../../template';
 
-const VERSION: string = version;
+const LUX_VERSION: string = version;
+const BABEL_PRESET_VERSION: string = devDependencies['babel-preset-lux'];
 
 /**
  * @private
@@ -21,19 +21,9 @@ export default (name: string): string => template`
     "license": "MIT",
     "dependencies": {
       "babel-core": "6.9.1",
-      "babel-plugin-external-helpers-2": "6.3.13",
-      "babel-plugin-syntax-trailing-function-commas": "6.8.0",
-      "babel-plugin-transform-async-to-generator": "6.8.0",
-      "babel-plugin-transform-class-properties": "6.9.1",
-      "babel-plugin-transform-decorators": "6.8.0",
-      "babel-plugin-transform-decorators-legacy": "1.3.4",
-      "babel-plugin-transform-es2015-destructuring": "6.9.0",
-      "babel-plugin-transform-es2015-parameters": "6.9.0",
-      "babel-plugin-transform-es2015-spread": "6.8.0",
-      "babel-plugin-transform-exponentiation-operator": "6.8.0",
-      "babel-plugin-transform-object-rest-spread": "6.8.0",
+      "babel-preset-lux": "${BABEL_PRESET_VERSION}",
       "knex": "0.11.5",
-      "lux-framework": "${VERSION}"
+      "lux-framework": "${LUX_VERSION}"
     }
   }
 `;
