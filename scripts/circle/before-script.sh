eaeb9eb983acdfe4159766871935d68b59bbc950
Zachary Golba <zachary.golba@postlight.com>
2016-10-03 16:12:30 -0400

    test: enable parallel circleci builds (#435)
    * test: enable parallel circleci builds
    
    * fix: infer parallelism in circle.yml
    
    * fix: expose circle node index to before-script.sh
    
    * fix: add parallel: true to test overrides in circle.yml

--- scripts/circle/before-script.sh
+++ scripts/circle/before-script.sh
@@ -2,19 +2,27 @@
 DROP_DATABASE="DROP DATABASE IF EXISTS lux_test;"
 CREATE_DATABASE="CREATE DATABASE lux_test;"
 
-case "$DATABASE_DRIVER" in
-  pg )
+case $CIRCLE_NODE_INDEX in
+  0)
+    export DATABASE_DRIVER="pg"
+
     psql -c "$DROP_DATABASE" -U postgres
     psql -c "$CREATE_DATABASE" -U postgres
     ;;
 
-  mysql2 )
+  1)
+    export DATABASE_DRIVER="mysql2"
+
     mysql -e "$DROP_DATABASE"
     mysql -e "$CREATE_DATABASE"
     ;;
 
-  sqlite3 )
+  2)
+    export DATABASE_DRIVER="sqlite3"
+
     rm -rf test/test-app/db/lux_test_test.sqlite
     touch test/test-app/db/lux_test_test.sqlite
     ;;
 esac
+
+echo "ENV: DATABASE_DRIVER=$DATABASE_DRIVER"
